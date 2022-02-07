231516fa85fcf58a23bc019666628fdc31a6353a
Martin Pucan <mpucan@trask.cz>
2022-02-07 15:05:52 +0100

    refactoring && disabling buttons && added state

--- src/App.js
+++ src/App.js
@@ -1,43 +1,76 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import './App.styles.scss';
 import {getWords} from "./data/words";
 
 function App() {
-    const originalArray = getWords()
-    const shuffleArray = originalArray.sort((a, b) => 0.5 - Math.random());
-    const [word, setWord] = useState(shuffleArray[0])
+  const shuffle = (arr) => {
+    return arr.sort(() => 0.5 - Math.random())
+  }
 
-    // TODO - need fix previous and next move - rather use useMemo()
+  const [visibleHint, setVisibleHint] = useState(false);
+  const [visibleLetter, setVisibleLetter] = useState(false);
+  const [backClick, setBackClick] = useState(true)
+  const [nextClick, setNextClick] = useState(false)
 
-    const moveBack = () => {
-        let i = shuffleArray.indexOf(word)
+  const shuffledCards = useMemo(() => {
+    const words = getWords()
+    return shuffle(words);
+  }, [])
 
-        if (i !== 0 && i < shuffleArray.length) {
-            setWord(shuffleArray[i - 1])
-        }
-    }
+  const [word, setWord] = useState(shuffledCards[0])
 
-    const moveNext = () => {
-        let i = shuffleArray.indexOf(word)
+  const moveBack = () => {
+    let index = shuffledCards.indexOf(word)
+    setNextClick(false)
 
-        if (i >= 0 && i < shuffleArray.length - 1) {
-            setWord(shuffleArray[i + 1])
-        }
-    }
+    index !== 0 && index < shuffledCards.length
+      ? setWord(shuffledCards[index - 1])
+      : setBackClick(true)
+  }
 
-    return (
-        <div className="container">
-            <header className="header">header</header>
-            <main className="main--wrapper">
-                <div>{word.english}</div>
-                <div>{word.czech}</div>
-            </main>
-            <div className="btn--wrapper">
-                <button className="btn" onClick={moveBack}>Back</button>
-                <button className="btn" onClick={moveNext}>Next</button>
-            </div>
+  const moveNext = () => {
+    let index = shuffledCards.indexOf(word)
+    setBackClick(false)
+
+    index < shuffledCards.length - 1
+      ? setWord(shuffledCards[index + 1])
+      : setNextClick(true)
+  }
+
+  const showAndHideTranslate = () => {
+    visibleLetter && setVisibleLetter(false)
+    visibleHint ? setVisibleHint(false) : setVisibleHint(true)
+  }
+
+  const getFirstLetterOfTranslate = () => {
+    visibleHint && setVisibleHint(false)
+    visibleLetter ? setVisibleLetter(false) : setVisibleLetter(true)
+  }
+
+  return (
+    <div className="container">
+      <header className="header"></header>
+      <main className="main--wrapper">
+        <div>{word.english}</div>
+        <div className="btn--hints">
+          <button className="btn" onClick={getFirstLetterOfTranslate}>Aa</button>
+          <button className="btn" onClick={showAndHideTranslate}>Hint</button>
         </div>
-    );
+        {visibleHint
+          ? <div>{word.czech}</div>
+          : <></>
+        }
+        {visibleLetter
+          ? <div>{word.czech.charAt(0).toUpperCase()}</div>
+          : <></>
+        }
+      </main>
+      <div className="btn--wrapper">
+        <button className="btn" onClick={moveBack} disabled={backClick}>Back</button>
+        <button className="btn" onClick={moveNext} disabled={nextClick}>Next</button>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default App
