8d573e8a0f78e5e1ba953988f59669d3403eac17
Martin Pucan <mpucan@trask.cz>
2022-02-08 17:56:04 +0100

    added words && fix height

--- src/data/words.js
+++ src/data/words.js
@@ -5,24 +5,100 @@ const words = [
     },
     {
         english: 'soul',
-        czech: 'duse'
+        czech: 'duše'
     },
     {
         english: 'huge',
-        czech: 'velky'
+        czech: 'obrovský'
     },
     {
         english: 'mark',
-        czech: 'oznacit'
+        czech: 'označit'
     },
     {
         english: 'tough',
-        czech: 'tezky'
+        czech: 'tvrdý'
     },
     {
         english: 'think',
-        czech: 'myslet'
+        czech: 'myslet si'
     },
+    {
+        english: 'despite',
+        czech: 'navzdory'
+    },
+    {
+        english: 'teaser',
+        czech: 'upoutávka'
+    },
+    {
+        english: 'dispatch',
+        czech: 'odeslání'
+    },
+    {
+        english: 'wiring',
+        czech: 'elektrické vedení'
+    },
+    {
+        english: 'concise',
+        czech: 'stručný'
+    },
+    {
+        english: 'encourage',
+        czech: 'podporovat'
+    },
+    {
+        english: 'briefly',
+        czech: 'Krátce'
+    },
+    {
+        english: 'frankly',
+        czech: 'upřímně řečeno'
+    },
+    {
+        english: 'freight',
+        czech: 'náklad'
+    },
+    {
+        english: 'dispatch',
+        czech: 'odeslání'
+    },
+    {
+        english: 'immediately',
+        czech: 'ihned'
+    },
+    {
+        english: 'maintain',
+        czech: 'udržovat'
+    },
+    {
+        english: 'smooth',
+        czech: 'hladký'
+    },
+    {
+        english: 'snooze',
+        czech: 'podřimovat'
+    },
+    {
+        english: 'charge',
+        czech: 'nabít'
+    },
+    {
+        english: 'accomplish',
+        czech: 'splnit'
+    },
+    {
+        english: 'wide',
+        czech: 'široký'
+    },
+    {
+        english: 'reduce',
+        czech: 'snížit'
+    },
+    {
+        english: 'aware',
+        czech: 'vědomý'
+    }
 ]
 
 export function getWords() {
